f63294b9e7e24c2d557a740048de7be3e09e7d45
mukulsoftwap <mukulsoftwap@gmail.com>
2016-12-29 10:26:41 +0000

    removeQueue

--- server/api/controllers/UserController.js
+++ server/api/controllers/UserController.js
@@ -7,32 +7,40 @@
 
 module.exports = {
 	addQueue : function(req,res){
-		User.findOne({devId : req.body.devId}).exec(function(err,val){
-			if(val!=undefined){
-
+		User.findOne({devId : req.body.devId}).exec(function(err,user){
+			console.log(user);
+			if(user!=undefined){
+				return res.ok(null);
 			}else{
-				req.body['code'] = ""
-				User.create(req.body).exec(function(err,res){
+				User.create(req.body).exec(function(err,create){
 					if(res){
-
+						updateQueue(req.body);
 					}
 				});
 			}
 		});
 	},
-	Queue : function(req,res){
-		User.findOne({deconId : req.body.deconId}).exec(function(err,val){
+	updateQueue : function(data){
+		Queue.findOne({deconId : data.deconId}).exec(function(err,val){
 			if(val!=undefined){
-				User.update({cQueue : req.body.current}).exec(function(err,val){
-				
-				});
-			}else
-				User.create(req.body).exec(function(err,res){
-					if(res){
-
-					}
+				console.log("already exixt");
+			}else{
+				data['cQueue'] = '5';
+				Queue.create(data).exec(function(err,val){
+					console.log(val);
 				});
 			}
+		});
+	},
+	getQueue : function(req,res){
+		console.log(req.params);
+		User.find({devId : req.params.id}).exec(function(err,val){
+			if(val.length>0) res.ok(val);
+		});
+	},
+	removeQueue : function(req,res){
+		User.destroy({devId : req.body.devId,beconId : req.body.beconId}).exec(function(err,val){
+			if(val) res.ok(val);
 		});
 	}
 };
